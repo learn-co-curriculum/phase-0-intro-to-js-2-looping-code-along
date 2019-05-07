b771df9939e885ab1aa7d312b89b7e0fb3b533b4
maxwellbenton <maxwellbenton@gmail.com>
2019-05-07 17:58:53 -0400

    fixed function name

--- test/indexTest.js
+++ test/indexTest.js
@@ -7,10 +7,10 @@ describe( 'index.js', () => {
     spy = sinon.spy( console, 'log' );
   } );
 
-  describe( 'writeCard()', () => {
+  describe( 'writeCards()', () => {
 
     it( 'returns an array of thank you messages for each name provided to the function', () => {
-      expect( writeCard( [ 'Lisa', 'Kaitlin', 'Jan' ], 'surprise' ) )
+      expect( writeCards( [ 'Lisa', 'Kaitlin', 'Jan' ], 'surprise' ) )
         .to.deep.eq( [ 'Thank you, Lisa, for the wonderful surprise gift!', 'Thank you, Kaitlin, for the wonderful surprise gift!', 'Thank you, Jan, for the wonderful surprise gift!' ] );
     } );
   } );
