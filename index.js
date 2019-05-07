0a1fc02230fa6e0bd86d2736dbd7ff7a058bde75
maxwellbenton <maxwellbenton@gmail.com>
2019-05-07 18:11:24 -0400

    adds updated solution

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
-// Code your solutions in this file
-function printBadges (names) {
-  for (let i = 0; i < names.length; i++) {
-    console.log(`Welcome ${names[i]}! You are employee #${i + 1}.`);
+function writeCards( namesArray, event ) {
+  let thankYouCards = []
+  for ( let i = 0; i < namesArray.length; i++ ) {
+    thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
   }
-
-  return names;
+  return thankYouCards
 }
 
-function tailsNeverFails () {
-  let tails = 0;
-
-  while (Math.random() >= 0.5) {
-    tails++;
+function countdown( countdown ) {
+  while ( countdown > 0 ) {
+    console.log( countdown );
+    countdown -= 1;
   }
-
-  return `You got ${tails} tails in a row!`;
+  console.log( countdown );
 }
