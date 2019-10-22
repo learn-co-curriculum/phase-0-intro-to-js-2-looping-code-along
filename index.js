c102c920a46d67d2623d4ada4cfb80c112ee53f5
Steven G. Harms <steven.harms@flatironschool.com>
2019-10-22 13:28:43 -0400

    Ensure parameter is not named as function name

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ function writeCards( namesArray, event ) {
   return thankYouCards
 }
 
-function countDown( countdown ) {
-  while ( countdown > 0 ) {
-    console.log( countdown );
-    countdown -= 1;
+function countDown( startingNumber ) {
+  while ( startingNumber > 0 ) {
+    console.log( startingNumber );
+    startingNumber -= 1;
   }
-  console.log( countdown );
+  console.log( startingNumber );
 }
