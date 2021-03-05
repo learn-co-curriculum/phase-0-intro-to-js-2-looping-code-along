a03f8f3da3cff1980a0dcbb7b2da0bd202bbd6a9
Hpollard003 <hpollard003@gmail.com>
2021-03-05 14:39:08 -0500

    Done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+// Code your solutions in this f
+let name = ['Hunter', 'sinon', 'Percey']
+
+function writeCards(name , event) {
+    let messages = []
+    for (let i = 0; i < name.length; i++) {
+        messages.push (`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    }
+    return messages
+}
+
+function countDown(number) {
+    let i = 0
+    while (i <= number) {
+        console.log(number--)
+    }
