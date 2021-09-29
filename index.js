4712cf4ef6b789feb782ad7b0fee448f9c8d136d
Adam <bosak3@gmail.com>
2021-09-29 14:23:30 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+
+function writeCards(names, event){
+    let messages = []
+    for (let i = 0; i < names.length; i ++){
+        messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    }
+    return messages;
+}
+
+
+function countDown(positiveNumber){
+    while(positiveNumber >= 0) {
+        console.log(positiveNumber);
+        positiveNumber --;
+    }
+}
