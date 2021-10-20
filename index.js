ccfdf4655b2e3350c98c2a3e0269060c01185e34
Adam <bosak3@gmail.com>
2021-10-20 12:01:53 -0500

    commit tree 9e1de6dd3ee6af57c9aaff1438cf7cacef654202

--- index.js
+++ index.js
@@ -1,16 +1,45 @@
 
+const gifts = ['teddy bear', 'drone', 'doll']
+// //For loop example
+// function wrapGifts(gifts) {
+//     for (let i = 0; i < gifts.length; i++) {
+//         console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//     }
+//     return gifts;
+// }
+//
+// wrapGifts(gifts);
+
+
+// //While loop example
+// function wrapGifts(gifts){
+//     let i = 0; 
+//     while (i < gifts.length){
+//         console.log(`Wrapped ${gifts[i]} and added a bow!`)
+//         i++;
+//     }
+//     return gifts;
+// }
+//
+// wrapGifts(gifts);
+
+
+//For loop 
 function writeCards(names, event){
     let messages = []
-    for (let i = 0; i < names.length; i ++){
-        messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
     }
     return messages;
 }
 
+writeCards(['Adam', 'Brooke', 'Nick', 'JJ'], "birthday");
 
-function countDown(positiveNumber){
-    while(positiveNumber >= 0) {
-        console.log(positiveNumber);
-        positiveNumber --;
+function countDown(num){
+    while ( num >= 0){
+    console.log(num);
+    num --;
     }
 }
+
+countDown(10);
