88602b5ade8da237df2577cef08986db0acfca00
Gracie <m.graciemcguire@gmail.com>
2021-12-02 12:17:49 -0500

    fixes readme and tests for inclusivity, adjusts based on linter rules

--- test/indexTest.js
+++ test/indexTest.js
@@ -14,8 +14,11 @@ describe( 'index.js', () => {
   describe( 'writeCards()', () => {
 
     it( 'returns an array of thank you messages for each name provided to the function', () => {
-      expect( writeCards( [ 'Lisa', 'Kaitlin', 'Jan' ], 'surprise' ) )
-        .to.deep.eq( [ 'Thank you, Lisa, for the wonderful surprise gift!', 'Thank you, Kaitlin, for the wonderful surprise gift!', 'Thank you, Jan, for the wonderful surprise gift!' ] );
+      expect(writeCards(["Guadalupe", "Ollie", "Aki"], "surprise")).to.deep.eq([
+        "Thank you, Guadalupe, for the wonderful surprise gift!",
+        "Thank you, Ollie, for the wonderful surprise gift!",
+        "Thank you, Aki, for the wonderful surprise gift!",
+      ]);
     } );
   } );
 
