f8b8b6230f144f24ecb9178ddc956ba8acb21565
Israa Qaba <israa.qaba98@gmail.com>
2022-04-18 20:48:03 +0300

    completed assigment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+const event = 'surprise';
+
+function writeCards(names, event) {
+    let newArr = [];
+    for (let i =0; i < names.length; i++) {
+        newArr.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return newArr;
+}
+
+function countDown(positiveNumber){
+    while(positiveNumber >= 0) {
+        console.log(positiveNumber);
+        positiveNumber--;
+    }
+
+}
