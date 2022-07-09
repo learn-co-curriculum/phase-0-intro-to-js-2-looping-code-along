12c878ba372626cda0ecf8cb2f9ba71ef0084d91
katya2108 <katerinavodeiko@gmail.com>
2022-07-08 23:33:07 -0600

    commit tree bef756c30fe38b047272253b47b7a51fb73173c7

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    let thankYouCards = [];
+    for (let i = 0; i < names.length; i++) {
+      thankYouCards.push(
+        `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+      );
+    }
+    return thankYouCards;
+  }
+ 
+  function countDown(countdown) {
+    while (countdown > 0) {
+      console.log(countdown);
+      countdown -= 1;
+    }
+    console.log(countdown);
+  }
