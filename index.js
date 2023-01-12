fba5e6aa3130620345f99b0e137d4b75889430e8
Alexander <alexanderguest@me.com>
2023-01-11 20:13:38 -0500

    commit tree 5cff24f57f6d2eb6a0b9dd04a84411ef5759832a

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+ 
+  }
+   
+  const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
